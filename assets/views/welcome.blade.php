b00e8d18ef44fa36df7740f18e5a7d51c5caa0c7
Mike Bronner <mike@genealabs.com>
2015-07-07 19:34:22 -0700

    Tweak views

--- assets/views/welcome.blade.php
+++ assets/views/welcome.blade.php
@@ -1,36 +1,8 @@
 @extends('app')
 
 @section('content')
-<html>
-	<head>
-		<title>Laravel</title>
-
-
-		<style>
-			.content {
-                color: #B0BEC5;
-				text-align: center;
-                font-weight: 100;
-                font-family: 'Lato';
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
-</html>
+    <div class="container">
+        <h1 class="page-header">Laravel 5</h1>
+        <blockquote class="quote">{{ Inspiring::quote() }}</blockquote>
+    </div>
 @stop
