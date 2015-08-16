958858f63d39dd2ac48d9a9d71eff7c4f6da94a2
Mike Bronner <mike@genealabs.com>
2015-08-16 09:13:24 -0700

    Add original code to reset and password views

--- assets/views/auth/reset.blade.php
+++ assets/views/auth/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('app')
 
 @section('content')
     <div class="container-fluid">
@@ -7,13 +7,50 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Reset Password</div>
                     <div class="panel-body">
-                        {!! Form::bs_open(['route' => 'password-reset', 'class' => 'form-horizontal', 'role' => 'form'], $errors, 4, 6) !!}
-                        {!! Form::hidden('token', $token) !!}
-                        {!! Form::bs_email('E-Mail Address', 'email', old('email'), ['class' => 'form-control']) !!}
-                        {!! Form::bs_password('Password', 'password', ['class' => 'form-control']) !!}
-                        {!! Form::bs_password('Confirm Password', 'password_confirmation', ['class' => 'form-control']) !!}
-                        {!! Form::bs_submit('Reset Password', null, ['class' => 'btn btn-primary']) !!}
-                        {!! Form::close() !!}
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <input type="hidden" name="token" value="{{ $token }}">
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">E-Mail Address</label>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Password</label>
+                                <div class="col-md-6">
+                                    <input type="password" class="form-control" name="password">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Confirm Password</label>
+                                <div class="col-md-6">
+                                    <input type="password" class="form-control" name="password_confirmation">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Reset Password
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
